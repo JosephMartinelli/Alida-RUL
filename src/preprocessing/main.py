afcf361d7c29226f569e3fc74cc645e4e97cef8a
GiuseppeM <peppemartinelli66@gmail.com>
2025-08-01 16:26:52 +0200

    testing ALIDA's inputs

--- src/preprocessing/main.py
+++ src/preprocessing/main.py
@@ -41,11 +41,12 @@ from minio import Minio, S3Error
 
 
 if __name__ == "__main__":
-    logging.warning(
-        "input_dataset: %s,input_minio_bucket: %s,input_minio_url: %s, input_access_key: %s, input_secret_key: %s",
-        args.input_dataset,
-        args.input_minio_bucket,
-        args.input_minio_url,
-        args.input_access_key,
-        args.input_secret_key,
-    )
+    while True:
+        logging.warning(
+            "input_dataset: %s,input_minio_bucket: %s,input_minio_url: %s, input_access_key: %s, input_secret_key: %s",
+            args.input_dataset,
+            args.input_minio_bucket,
+            args.input_minio_url,
+            args.input_access_key,
+            args.input_secret_key,
+        )
